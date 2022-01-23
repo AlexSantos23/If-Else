f9d2ec5e08b888079c407dd2be8cfecdb8ec0920
Alex <alexsantosdasilva1133@gmail.com>
2022-01-23 17:18:23 -0300

    commit

---
+++ Documents/Projetos VNW/ForWhile/script.js
@@ -0,0 +1,17 @@
+for(let i = 1; i <=  10; i ++){
+    console.log(i)
+}
+
+let contador = 10
+while(contador >= 1){
+    console.log(contador)
+    contador--
+}
+
+for(let i = 1; i < 100; i = i+2){
+    console.log(i)
+}
+
+for(let i = 0; i <= 100; i = i+2){
+    console.log(i)
+}
