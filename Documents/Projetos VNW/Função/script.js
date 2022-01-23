b12e6a4ff2ec565e91f35370268c063b6aaa7cde
Alex <alexsantosdasilva1133@gmail.com>
2022-01-23 18:26:30 -0300

    commit

---
+++ Documents/Projetos VNW/Função/script.js
@@ -0,0 +1,20 @@
+function texto(){
+    console.log(`mensagem teste`)
+}
+texto()
+
+let name = "Alex"
+function mostrarNome(batata){
+    console.log(`Me chamo ${batata}, prazer.`)
+}
+mostrarNome(name)
+
+function dados(nome, número, estilo){
+    console.log(`${nome}, ${número}, ${estilo}`)
+}
+dados(`Alex`, `23`, `rock`)
+
+function acervo(filme, música){
+    console.log(`${filme}, ${música}`)
+}
+acervo(`KillBill`, `Dead Weight`)
