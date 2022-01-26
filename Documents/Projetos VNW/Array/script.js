b148920daccd7941434380c1da909e895ec8b9cb
Alex <alexsantosdasilva1133@gmail.com>
2022-01-26 12:53:58 -0300

    array

---
+++ Documents/Projetos VNW/Array/script.js
@@ -0,0 +1,26 @@
+let fruits = ["banana", "pineaple", "apple", "jackfruit", "grape"]
+console.log(fruits)
+
+fruits.unshift("orange")
+console.log(fruits)
+
+fruits.pop()
+console.log(fruits)
+
+fruits.push("green apple", "watermelon")
+console.log(fruits)
+
+fruits.shift()
+console.log(fruits)
+
+let numeros = [7,5,6,3,8,9,2,1,4]
+
+let numerosOrganizados = numeros.sort(function(a,b){
+    return a-b
+})
+console.log(numeros)
+
+
+
+
+
